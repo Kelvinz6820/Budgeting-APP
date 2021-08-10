e0fa1da251ad6c8a17b97eda9b8ee2344b08dbd2
Kelvin Zheng <kzheng000@citymail.cuny.edu>
2021-08-10 09:52:56 -0400

    added front end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+//PAGES
+import NavBar from "./Pages/Navbar"
+import Transactions from './Pages/Transactions'
+import axios from "axios";
+import NavBar from "./Components/NavBar";
+import Transactions from "./Pages/"
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">
+
+    <NavBar/>
+  </div>;
 }
 
 export default App;
